4711a417e02323fb558a74ee20cfc6b8b6e95e6a
ProximaNova <jaretrudd@gmail.com>
2023-06-27 21:22:12 -0600

    Update iaipfs.py

--- iaipfs.py
+++ iaipfs.py
@@ -13,13 +13,28 @@ from os.path import exists
 # import time
 import re
 
+# -- Run "./iaipfs.py -h" for help text.
+# -- ./iaipfs.py -h might say "optional arguments"
+# -- on the third line of output. That is is incorrect,
+# -- for only -p is optional.
+
 parser = argparse.ArgumentParser()
-# -- folder to add
-parser.add_argument("-f", "--folder", type=str)
-# -- cid log
-parser.add_argument("-c", "--cidlog", type=str)
-# -- item index
-parser.add_argument("-x", "--htmlindex", type=str)
+# -- pin or not, folder to add, cid log, item index
+parser.add_argument("-p", "--pin", action="store_true",
+    required=False, help="""(optional) if specified,
+    this pins the CID; pin it if you want to keep the
+    data for a long time""")
+parser.add_argument("-f", "--folder", type=str,
+    required=True, help="""path to the folder which only
+    contains data from one IA item, the folder's name
+    should be the item identifier""")
+parser.add_argument("-c", "--cidlog", type=str,
+    required=True, help="""initially empty text file to
+    log the CIDs in""")
+parser.add_argument("-x", "--htmlindex", type=str,
+    required=True, help="""initially empty text file to
+    contain some HTML data which acts as an index of IA
+    items in IPFS""")
 args = parser.parse_args()
 folder = args.folder
 folderfolder = os.path.dirname(folder)
@@ -28,8 +43,6 @@ folderlast = folderpath.name
 cidlog = args.cidlog
 htmlindex = args.htmlindex
 tempfile = cidlog + ".1686364707.temp"
-
-# with open(cidlog, 'w') as sys.stdout:
 
 print("Folder: " + folder)
 print("CID log: " + cidlog)
@@ -50,6 +63,7 @@ with open(tempfile, "r") as file:
 file1 = open(cidlog, "a")
 file1.write(data1 + " = " + data2 + "\n")
 file1.close()
+# -- dedup cid
 file1 = open(cidlog, "r")
 file2 = open(tempfile, "w")
 line_dedup = set()
@@ -72,6 +86,7 @@ with open(tempfile, "r") as file:
 cmd="ipfs cat " + data1 + "/" + folderlast + "_meta.xml > " + tempfile
 os.system(cmd)
 
+subject = ""
 with open(tempfile, "r") as file3:
     for line5 in file3:
         match = re.search("<title>.*", line5)
@@ -79,11 +94,32 @@ with open(tempfile, "r") as file3:
             title = re.findall("<title>.*", line5)
         match2 = re.search("<subject>.*", line5)
         if match2:
-            subject = re.findall("<subject>.*", line5)
+            subject += str(re.findall("<subject>.*", line5))
 file3.close()
 title2 = re.sub("\[?'?<\/?title>'?\]?", "", str(title))
-subject2 = re.sub("\[?'?<\/?subject>'?\]?", "", str(subject))
+subject2 = re.sub("<\/subject>'\]\['<subject>", "; ", str(subject))
+subject3 = re.sub("\[?'?<\/?subject>'?\]?", "", str(subject2))
 
 file4 = open(htmlindex, "a")
-file4.write("<li>" + folderlast + ": <a href=\"ipfs://" + data2 + "\">" + title2 + "</a> - " + subject2 + "</li>\n")
+file4.write("<li>" + folderlast + ": <a href=\"ipfs://" + data2 + "\">" + title2 + "</a> - " + re.sub(";", ",", subject3) + "</li>\n")
 file4.close()
+# -- dedup html
+file1 = open(htmlindex, "r")
+file2 = open(tempfile, "w")
+line_dedup = set()
+for line in file1:
+    lines1 = line.rstrip().encode('utf-8')
+    if lines1 not in line_dedup:
+        file2.write(line)
+        line_dedup.add(lines1)
+file1.close()
+file2.close()
+with open(tempfile, "r") as file:
+    data3 = file.read()
+    file1 = open(htmlindex, "w")
+    file1.write(data3)
+    file1.close()
+
+if args.pin:
+    cmd="ipfs pin add " + data1
+    os.system(cmd)
